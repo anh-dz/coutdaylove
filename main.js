9f754e6a68e602da0486f1fbbe96af9a2d6d5d72
Mink Alex Vina <64893437+anh-dz@users.noreply.github.com>
2022-12-30 11:24:44 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -4,8 +4,15 @@ document.addEventListener('DOMContentLoaded', function() {
   // var min = document.querySelector('.min');
   // var second = document.querySelector('.second');
 
-  var startDate = new Date(2021, 9, 20); //Month-=1
-  days.innerText = Math.floor((new Date - startDate)/86400000);
+  var startDate = new Date(2023, 0, 1); //Month-=1
+  var day_count = Math.floor((new Date - startDate)/86400000);
+  if (day_count <= 0)
+    day_count = "00 DAY"
+  else if (day_count == 1)
+    day_count+=" DAY"
+  else
+    day_count+=" DAYS"
+  days.innerText = day_count 
   // countTime();
 
   // function countTime() {
@@ -16,6 +23,6 @@ document.addEventListener('DOMContentLoaded', function() {
   //   second.innerText = Math.floor(ms % 3600000 % 60000 / 1000);
   // }
 
-  setInterval(countTime, 1000);
+  // setInterval(countTime, 1000);
 
 }, false);
